7af29f18066bb61db21e7057558f4e9a7a727084
Jason Wong <wongjason415@gmail.com>
2022-02-22 21:43:43 -0800

    Rock paper scissors game without styling

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//Computer to generate rock, paper or scissors randomly
 function computerPlay () {
-   let computerSelection = Math.floor(Math.random() * 3);
+   computerSelection = Math.floor(Math.random() * 3);
 
     switch (computerSelection) {
         case 0:
@@ -15,61 +14,87 @@ function computerPlay () {
             computerSelection = "SCISSORS";
             break;
     }
-    return computerSelection;
 }
 
-//Ask user for rock, paper or scissors input (case insensitive)
-//Play the round and compare user input to computer selection
-//Output win or lose and condition statement
+function playerChoice (element) {
+    playerSelection = element.innerHTML.toUpperCase();
+}
+
 function playRound (playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        return;
 
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        return "You lose! Paper beats rock.";
+        return j++;
 
     } else if (playerSelection === "ROCK" && computerSelection ==="SCISSORS") {
-        j++;
-        return "You win! Rock beats scissors.";
+        return i++;
 
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        j++;
-        return "You win! Paper beats rock.";
+        return i++;
     
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        return "You lost! Scissors beats paper.";
+        return j++;
 
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        return "You lost! Rock beats sciossors.";
+        return j++;
         
     } else {
-        j++;
-        return "You win! Scissors beats paper.";
+        return i++;
+    }
+} 
+
+//display character selection inside div with id="area"
+function displaySelections (container, playerSelection, computerSelection) {
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "selection");
+    const newContent = document.createTextNode(
+        "You chose " + playerSelection + " and the computer chose " + computerSelection + ". It is " + i + " to " + j + ".");
+    newDiv.appendChild(newContent);
+    container.appendChild(newDiv);
+}
+
+function displayWinner (i, j) {
+    if (i == 5) {
+        setTimeout(() => alert("You won " + i + " to " + j + "!"), 1);
     }
     
-}
-
-//play 5 games where player imput is compared to computer selection
-//
-//
-function game() {
-    for (var i = 0; i < 5; i++) {
-        let playerSelection = window.prompt("Enter: Rock, Paper, or Scissors");
-        playerSelection = playerSelection.toUpperCase();
-
-        if (playerSelection == "ROCK" || playerSelection == "PAPER" || playerSelection == "SCISSORS") {
-            let computerSelection = computerPlay();
-
-            console.log(playRound(playerSelection, computerSelection));
-        
-        } else {
-            return "Please enter rock, paper, or scissors.";
-        }
+    else {
+        setTimeout(() => alert("You lost " + i + " to " + j + "!"), 1);
     }
-
-    return "You won " + j + " times!";
 }
 
+let i = 0;
 let j = 0;
-console.log(game());
+let playerSelection = "string";
+let computerSelection = "string";
+
+//create container
+var container = document.createElement("div");
+container.classList.add("container");
+
+//append container to div with id="area"
+const element = document.getElementById("area");
+element.appendChild(container);
+
+//add event listener to the button and 
+//play rock, paper, scissors when the button is clicked
+const buttonElement = document.getElementsByClassName("btn");
+for (var k = 0; k < buttonElement.length; k++) {
+    buttonElement[k].addEventListener("click", function (e) {
+        playerChoice(e.target);
+        computerPlay(e.target);
+        playRound(playerSelection, computerSelection);
+        displaySelections(container, playerSelection, computerSelection);
+        
+        if (i == 5 || j == 5) {
+            displayWinner(i, j);
+            
+            //reset the game
+            i = 0;
+            j = 0;
+            container.innerHTML = "";
+        }
+    })
+}
